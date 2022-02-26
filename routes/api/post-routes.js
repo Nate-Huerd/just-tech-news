a3d8a251e378350f6527da9d76181b8d20f120ca
Nate-Huerd <nhuerd3@gmail.com>
2022-02-26 00:49:18 -0600

    13.3 code

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -2,21 +2,23 @@ const router = require('express').Router();
 const { Post, User } = require('../../models');
 
 // get all users
-Post.findAll({
-  attributes: ['id', 'post_url', 'title', 'created_at'],
-  order: [['created_at', 'DESC']], 
-  include: [
-    {
-      model: User,
-      attributes: ['username']
-    }
-  ]
-})
-  .then(dbPostData => res.json(dbPostData))
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+router.get('/', (req, res) => {
+  Post.findAll({
+    attributes: ['id', 'post_url', 'title', 'created_at'],
+    order: [['created_at', 'DESC']],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 router.get('/:id', (req, res) => {
   Post.findOne({
